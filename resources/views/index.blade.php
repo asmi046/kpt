7d9b12210cefcf68e408f9c8476e35fb40bff1ca
asmi046 <asmi046@gmail.com>
2025-02-11 14:53:53 +0300

    Оформление верстки

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,5 +10,27 @@
 
 @section('main')
 
+<header class="main_header">
+    <div class="container">
+        <div class="left_side">
+            <div class="top">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('img/logo.svg') }}" alt="{{ config('lend_data.company_name') }}">
+                </a>
+
+                <nav>
+                    <ul>
+                        <li><a href="#">О нас</a></li>
+                        <li><a href="#">Услуги</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                </nav>
+            </div>
+        
+        </div>
+        
+    </div>
+</header>
+
 @endsection
 
